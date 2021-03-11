06ca15d401d02688f1b46e20dc5f7e3393dcdfcc
levkopo <40952805+levkoposc@users.noreply.github.com>
2021-03-11 18:52:49 +0500

    new version

--- src/main/java/com/github/kayjamlang/executor/cli/Main.java
+++ src/main/java/com/github/kayjamlang/executor/cli/Main.java
@@ -1,14 +1,20 @@
 package com.github.kayjamlang.executor.cli;
 
 import com.github.kayjamlang.executor.Executor;
-import com.github.kayjamlang.executor.MainLibrary;
+import com.github.kayjamlang.executor.libs.main.MainLibrary;
+import com.github.kayjamlang.executor.libs.Library;
 import org.apache.commons.cli.*;
 import org.mozilla.universalchardet.UniversalDetector;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Paths;
+import java.util.Enumeration;
 import java.util.Scanner;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class Main {
 
@@ -27,18 +33,49 @@ public class Main {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
-            formatter.printHelp("kayjam", options);
+            formatter.printHelp("kayjam-cli", options);
 
             System.exit(1);
             return;
         }
 
-        File executeFile = new File(Paths.get(System.getProperty("user.dir"),
-                cmd.getOptionValue("file")).normalize().toString());
-
         Executor executor = new Executor();
         executor.addLibrary(new MainLibrary());
+        File libDir = new File(Paths.get(System.getProperty("user.dir"),
+                "./libs/").normalize().toString());
+        if(libDir.exists()){
+            File[] files = libDir.listFiles();
+            if(files!=null)
+                for(File libJar: files){
+                    if(libDir.getName().endsWith(".jar"))
+                        loadJarLibrary(executor, libJar);
+                }
+        }
+
+        File executeFile = new File(Paths.get(System.getProperty("pathFile"),
+                cmd.getOptionValue("file")).normalize().toString());
         executor.execute("{"+read(executeFile)+"}");
+    }
+
+    private static void loadJarLibrary(Executor executor, File file) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        JarFile jarFile = new JarFile(file);
+        Enumeration<JarEntry> e = jarFile.entries();
+
+        URL[] urls = { new URL("jar:file:" + file.getPath()+"!/") };
+        URLClassLoader cl = URLClassLoader.newInstance(urls);
+
+        while (e.hasMoreElements()) {
+            JarEntry je = e.nextElement();
+            if(je.isDirectory() || !je.getName().endsWith(".class")){
+                continue;
+            }
+
+            String className = je.getName().substring(0,je.getName().length()-6);
+            className = className.replace('/', '.');
+            Class<?> c = cl.loadClass(className);
+            if(c.isAssignableFrom(Library.class))
+                executor.addLibrary((Library) c.newInstance());
+        }
     }
 
     private static String read(File file) throws IOException {
